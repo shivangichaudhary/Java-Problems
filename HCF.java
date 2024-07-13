ce51b67ad505b0a9da41cb2bbaa92aab6efbe820
shivangichaudhary <99113890+shivangichaudhary@users.noreply.github.com>
2024-07-13 17:40:16 +0530

    Add files via upload

---
+++ HCF.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+//        System.out.println("Enter the first number: ");
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//
+//        System.out.println("Enter the second number: ");
+//        Scanner st = new Scanner(System.in);
+//        int b = st.nextInt();
+        int a = 3;
+        int b = 7;
+        int i;
+
+
+    }
+}
